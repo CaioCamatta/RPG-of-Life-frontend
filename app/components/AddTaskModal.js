4412ff4ba83b858fdfdd669d87632c0ddf48b713
Jack <jahill@live.ca>
2021-01-08 17:02:40 -0500

    tasks with add+remove, no link to db

--- app/components/AddTaskModal.js
+++ app/components/AddTaskModal.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Form } from "react-bootstrap";
 import styles from "./addTaskModal.module.css";
 
 export default class AddTaskModal extends Component {
@@ -9,20 +9,33 @@ export default class AddTaskModal extends Component {
 
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.props.handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.props.handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={this.props.handleClose}>
-            Save (just closes)
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      
+        <Modal show={this.props.show} onHide={this.props.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Create a Task</Modal.Title>
+          </Modal.Header>
+          <Form id="add-task-form" onSubmit={this.props.handleSubmit}className="m-5">
+            <Form.Control name="name" placeholder="Task name" />
+            <Form.Label className="mt-4">
+              Category
+            </Form.Label>
+            <Form.Control name="stat" as="select">
+              <option>Health</option>
+              <option>Strength</option>
+              <option>Intelligence</option>
+              <option>Creativity</option>
+              <option>Charisma</option>
+            </Form.Control>
+          </Form>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleClose}>
+              Cancel
+            </Button>
+            <Button variant="primary" type="submit" form="add-task-form" onClick={this.props.handleClose}>
+              Add
+            </Button>
+          </Modal.Footer>
+        </Modal>
     );
   }
 }
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,17 +1,72 @@
 import { Component } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col, Button, ListGroup } from "react-bootstrap";
 import styles from "./home.module.css";
 import { PROFILE, SHOP, FRIENDS } from "./App";
 import AddTaskModal from "./AddTaskModal";
 
+var tasks = [
+  "Eat Vegetables"
+]
+
+var statIcons = {
+  "Health":"https://img.icons8.com/material-sharp/24/000000/like--v1.png",
+  "Strength":"https://img.icons8.com/ios-filled/50/000000/dumbbell.png",
+  "Intelligence":"https://img.icons8.com/ios-glyphs/30/000000/open-book--v1.png",
+  "Creativity":"https://img.icons8.com/ios-glyphs/24/000000/paint.png",
+  "Charisma":"https://img.icons8.com/pastel-glyph/64/000000/groups--v4.png"
+}
+
+var taskList = [];
+
 export default class Home extends Component {
+
   constructor(props) {
     super(props);
 
     this.state = {
       showAddTaskModal: false,
     };
+
+    taskList = [];
+
+    for (let i = 0; i < tasks.length; i++) {
+      taskList.push({
+        name: tasks[i],
+        stat: "Health"
+      });
+    }
+
+    this.state = { taskList };
   }
+
+  handleComplete = (index) => {
+    delete taskList[index];
+    this.setState({ taskList: taskList });
+  };
+
+  handleSubmit = evt => {
+    evt.preventDefault();
+    taskList.push({
+      name: evt.target.name.value,
+      stat: evt.target.stat.value
+    })
+    this.setState({ taskList: taskList });
+    //making a post request with the fetch API
+    /*fetch('/server', {
+        method: 'POST',
+        headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            firstName:this.state.firstName
+          })
+        })
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))
+    });*/
+  };
 
   handleAddTaskModalToggle = () => {
     this.setState({ showAddTaskModal: !this.state.showAddTaskModal });
@@ -51,21 +106,31 @@ export default class Home extends Component {
             </div>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Button
-              variant="primary"
-              onClick={this.handleAddTaskModalToggle}
-              className="m-1 mt-5"
-            >
-              Add Task
-            </Button>
-            <AddTaskModal
-              show={this.state.showAddTaskModal}
-              handleClose={this.handleAddTaskModalToggle}
-            />
-          </Col>
-        </Row>
+        <div className="scroll-view">
+          <ListGroup>
+            {this.state.taskList.map((task, index) => (
+              <ListGroup.Item className="task-item">
+                <p>
+                  <img width="16" height="16" className="mr-2" src={ statIcons[task.stat] }/>
+                  { task.name }
+                </p>
+                <Button variant="success" size="sm" className="" onClick={() => this.handleComplete(index)}>Complete</Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Button
+            variant="primary"
+            onClick={this.handleAddTaskModalToggle}
+            className="m-3 mx-auto"
+          >
+            Add Task
+          </Button>
+          <AddTaskModal
+            show={this.state.showAddTaskModal}
+            handleClose={this.handleAddTaskModalToggle}
+            handleSubmit={this.handleSubmit}
+          />
+        </div>
       </Container>
     );
   }
