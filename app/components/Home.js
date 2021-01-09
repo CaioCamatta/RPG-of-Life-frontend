cff24af2e06dded66ffcdd7c3fade5431ed0a87d
Jack <jahill@live.ca>
2021-01-09 16:43:12 -0500

    Updated profile header and tasks layout

--- app/components/Home.js
+++ app/components/Home.js
@@ -109,106 +109,100 @@ export default class Home extends Component {
   render() {
     return (
       <div>
-        <Row>
-          <Col className="p-0">
-            <div className="py-3 px-3 profile-header w-100 d-flex">
-              <div className="pl-2 d-inline-block">
-                <Avatar />
-              </div>
-              <div className="mt-1 d-inline-block ml-2">
-                <p className="mb-1 font-weight-bold h5">
-                  {this.state.profile?.username ?? "Username"}
-                </p>
-                <p className="mb-0">XP {this.state.profile?.XP ?? "0"}</p>
-                <p className="mb-0">
-                  {" "}
-                  <FontAwesomeIcon icon={faCoins} className="mr-2" />
-                  {this.state.profile?.coins ?? "0"}
-                </p>
-                <p className="mb-0 stats-display">
-                  {this.state.profile && (
-                    <>
-                      <span className="mr-2">
-                        <FontAwesomeIcon icon={faPlusSquare} />{" "}
-                        {this.state.profile.stats.health}
-                      </span>
-                      <span className="mr-2">
-                        <FontAwesomeIcon icon={faDumbbell} />{" "}
-                        {this.state.profile.stats.strength}
-                      </span>
-                      <span className="mr-2">
-                        <FontAwesomeIcon icon={faBrain} />{" "}
-                        {this.state.profile.stats.intelligence}
-                      </span>
-                      <span className="mr-2">
-                        <FontAwesomeIcon icon={faPalette} />{" "}
-                        {this.state.profile.stats.creativity}
-                      </span>
-                      <span className="mr-2">
-                        <FontAwesomeIcon icon={faHandHoldingHeart} />{" "}
-                        {this.state.profile.stats.charisma}
-                      </span>
-                    </>
-                  )}
-                </p>
-
-                <div className="align-right mt-1">
-                  <Button
-                    onClick={() => this.props.navigate(FRIENDS)}
-                    outline
-                    variant="secondary"
-                    className="m-1 px-1 py-0 small"
-                  >
-                    Friends
-                  </Button>
-                  <Button
-                    onClick={() => this.props.navigate(SHOP)}
-                    outline
-                    variant="secondary"
-                    className="m-1 px-1 py-0 small"
-                  >
-                    Shop
-                  </Button>
-                  <Button
-                    onClick={() => this.props.navigate(PROFILE)}
-                    outline
-                    variant="secondary"
-                    className="m-1 px-1 py-0 small"
-                  >
-                    Profile
-                  </Button>
-                </div>
-              </div>
-            </div>
-          </Col>
-        </Row>
-        <Container>
-          <div className="scroll-view">
-            <ListGroup>
-              {this.state.taskList.map((task, index) => (
-                <ListGroup.Item className="task-item">
-                  <p>
-                    <img width="16" height="16" className="mr-2" src={ statIcons[task.stat] }/>
-                    { task.name }
-                  </p>
-                  <Button variant="success" size="sm" className="" onClick={() => this.handleComplete(index)}>Complete</Button>
-                </ListGroup.Item>
-              ))}
-            </ListGroup>
-            <Button
-              variant="primary"
-              onClick={this.handleAddTaskModalToggle}
-              className="m-3 mx-auto"
-            >
-              Add Task
-            </Button>
-            <AddTaskModal
-              show={this.state.showAddTaskModal}
-              handleClose={this.handleAddTaskModalToggle}
-              handleSubmit={this.handleSubmit}
-            />
+        <div className="p-3 profile-header w-100">
+          <div className="pl-2 d-inline-block">
+            <Avatar />
           </div>
-        </Container>
+          <div className="mt-1 d-inline-block ml-2">
+            <p className="mb-1 font-weight-bold h5">
+              {this.state.profile?.username ?? "Username"}
+            </p>
+            <p className="mb-0">XP {this.state.profile?.XP ?? "0"}</p>
+            <p className="mb-0">
+              {" "}
+              <FontAwesomeIcon icon={faCoins} className="mr-2" />
+              {this.state.profile?.coins ?? "0"}
+            </p>
+            <p className="mb-0 stats-display">
+              {this.state.profile && (
+                <>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faPlusSquare} />{" "}
+                    {this.state.profile.stats.health}
+                  </span>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faDumbbell} />{" "}
+                    {this.state.profile.stats.strength}
+                  </span>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faBrain} />{" "}
+                    {this.state.profile.stats.intelligence}
+                  </span>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faPalette} />{" "}
+                    {this.state.profile.stats.creativity}
+                  </span>
+                  <span className="mr-2">
+                    <FontAwesomeIcon icon={faHandHoldingHeart} />{" "}
+                    {this.state.profile.stats.charisma}
+                  </span>
+                </>
+              )}
+            </p>
+
+            <div className="align-right mt-1">
+              <Button
+                onClick={() => this.props.navigate(FRIENDS)}
+                outline
+                variant="secondary"
+                className="m-1 px-1 py-0 small"
+              >
+                Friends
+              </Button>
+              <Button
+                onClick={() => this.props.navigate(SHOP)}
+                outline
+                variant="secondary"
+                className="m-1 px-1 py-0 small"
+              >
+                Shop
+              </Button>
+              <Button
+                onClick={() => this.props.navigate(PROFILE)}
+                outline
+                variant="secondary"
+                className="m-1 px-1 py-0 small"
+              >
+                Profile
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="scroll-view">
+          <ListGroup>
+            {this.state.taskList.map((task, index) => (
+              <ListGroup.Item className="task-item">
+                <p>
+                  <img width="16" height="16" className="mr-2" src={ statIcons[task.stat] }/>
+                  { task.name }
+                </p>
+                <Button variant="success" size="sm" className="" onClick={() => this.handleComplete(index)}>Complete</Button>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+          <Button
+            variant="primary"
+            onClick={this.handleAddTaskModalToggle}
+            className="m-3 mx-auto"
+          >
+            Add Task
+          </Button>
+          <AddTaskModal
+            show={this.state.showAddTaskModal}
+            handleClose={this.handleAddTaskModalToggle}
+            handleSubmit={this.handleSubmit}
+          />
+        </div>
       </div>
     );
   }
