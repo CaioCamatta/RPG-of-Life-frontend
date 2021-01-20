99f09ac975e63d92278fba9be3438cf1b12927f2
Caio Camatta <caio@vidhug.com>
2021-01-19 21:08:24 -0500

    Style changes index

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Link from "next/link";
 import { Component } from "react";
-import { Button, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import Avatar from "../components/Avatar";
+import Image from "next/image";
 
 import {
   faCoins,
@@ -73,6 +74,26 @@ export default class Home extends Component {
                   and gain gold to buy items, or compete against your friends in
                   group challenges to be the Top Adventurer in your party
                 </p>
+                <div className="mt-5">
+                  <Row className="mx-0">
+                    <Col size="sm" className="text-right">
+                      <Image
+                        src="/ss1-min.png"
+                        alt="Picture of the author"
+                        width={188 * 1.5}
+                        height={333 * 1.5}
+                      />
+                    </Col>
+                    <Col size="sm">
+                      <Image
+                        src="/ss2-min.png"
+                        alt="Picture of the author"
+                        width={188 * 1.5}
+                        height={333 * 1.5}
+                      />
+                    </Col>
+                  </Row>
+                </div>
               </div>
             </div>
 
@@ -211,9 +232,22 @@ export default class Home extends Component {
             />
           </main>
         </Row>
+        <footer className="text-center pb-3 mt-5">
+          <p className="text-gray-500 sm:hidden block text-sm mt-3 small">
+            {" "}
+            Powered by{" "}
+            <a className="underline" href="https://nextjs.org/">
+              Next.js
+            </a>
+          </p>
+          <p className="text-sm text-gray-500 small">
+            © 2021 Caio Camatta Coelho, Caleb Sutherland, Daniel Wakefield, Jack
+            Hillier
+          </p>
+        </footer>
         <style jsx>{`
           main {
-            padding: 5rem 0;
+            margin-top: 3rem;
             flex: 1;
             display: flex;
             flex-direction: column;
@@ -223,7 +257,6 @@ export default class Home extends Component {
           .title {
             margin: 0;
             line-height: 1.15;
-            font-size: 4rem;
           }
 
           .description {
@@ -238,9 +271,11 @@ export default class Home extends Component {
             text-align: justify;
             font-size: 90%;
           }
-          p {
+          main p {
             word-wrap: break-word;
-            font-size: 1.2rem;
+            font-size: 1.15rem;
+            padding-left: 20px;
+            padding-right: 20px;
           }
           table {
             margin-left: auto;
@@ -254,11 +289,14 @@ export default class Home extends Component {
             font-size: 1.3rem;
           }
 
-          h1,
           h2,
           h3 {
             font-family: "breathefire";
-            font-size: 4rem;
+            font-size: 3rem;
+          }
+          h1 {
+            font-size: 4.5rem;
+            font-family: "breathefire";
           }
 
           #icon1 {
@@ -270,32 +308,37 @@ export default class Home extends Component {
             padding-right: 0;
           }
 
+          .container {
+            max-width: 880px !important;
+          }
+
           .makewater {
             position: absolute;
             width: 20%;
-            opacity: 0.2;
+            opacity: 0.1;
             float: none;
             padding: 0;
           }
           #makewater1 {
             right: 10%;
-            top: 30%;
+            top: 40%;
           }
           #makewater2 {
-            top: 60%;
+            left: 10%;
+            top: 42%;
           }
           #makewater3 {
             right: 10%;
-            bottom: -60%;
+            bottom: -70%;
           }
 
           #makewater4 {
             left: 10%;
-            bottom: -120%;
+            bottom: -70%;
           }
 
           #gs1 {
-            background-color: rgba(202, 202, 202, 0.527);
+            background-color: rgb(202 202 202 / 28%);
           }
 
           #gs2 ul {
@@ -316,20 +359,27 @@ export default class Home extends Component {
             font-size: 150%;
           }
 
-          @media only screen and (max-width: 343px) {
-            #icon1 {
-              position: relative;
-              top: -3rem;
-              right: -8rem;
-              margin: 0;
-              padding-right: 0;
-            }
-
-            #start {
+          @media only screen and (max-width: 500px) {
+            \ #start {
               width: 100%;
 
               padding-left: 2rem;
               padding-right: 2rem;
+            }
+
+            main {
+              text-align: center;
+            }
+
+            h1 {
+              font-size: 3rem;
+            }
+            h2,
+            h3 {
+              font-size: 2.5rem;
+            }
+            th {
+              font-size: 1rem;
             }
           }
 
